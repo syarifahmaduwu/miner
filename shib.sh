1b500d83c512928b49f626ec66d61f519ddce926
syarifahmaduwu <86199857+syarifahmaduwu@users.noreply.github.com>
2021-06-20 23:28:40 +0700

    Create shib.sh

---
+++ shib.sh
@@ -0,0 +1,5 @@
+# SHIB
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u SHIB:0x11bf91fba6007500f84410976ff5f96ed53a44b3.ahmd#0s40-jdkj -p x -k -a rx/0 --cpu 8
